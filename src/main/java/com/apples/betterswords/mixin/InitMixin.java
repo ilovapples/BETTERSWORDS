f643c62b51004e7e0c1a6a0deb9277172e239eb5
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-20 09:40:48 -0500

    Update InitMixin.java

--- src/main/java/com/apples/betterswords/mixin/InitMixin.java
+++ src/main/java/com/apples/betterswords/mixin/InitMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class InitMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		BetterSwords.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
