c2de8c4200375cc8845a15d0b85a5e06e26eadf4
ilovapples <89090508+ilovapples@users.noreply.github.com>
2023-04-20 09:22:28 -0500

    Update BetterSwords.java

--- src/main/java/com/apples/betterswords/BetterSwords.java
+++ src/main/java/com/apples/betterswords/BetterSwords.java
@@ -1,6 +1,7 @@
 package com.apples.betterswords;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class BetterSwords implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("betterswords");
 
 	public static final Spewing SPEWING = new Spewing(new FabricItemSettings());
 	
@@ -21,7 +22,7 @@ public class BetterSwords implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("ilovapples' Garbage BETTERSWORDS mod has been initialized!");
 		Registry.register(Registries.ENCHANTMENT, new Identifier("betterswords", "spewing"), SPEWING);
 	}
 }
